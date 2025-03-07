851adf91664c1b2669b0695bfc101dafae03c481
kaittah <kaitlynhennacy@gmail.com>
2025-03-07 11:10:33 -0800

    Redirect api

--- apps/web/src/hooks/utils.ts
+++ apps/web/src/hooks/utils.ts
@@ -1,8 +1,9 @@
 import { Client } from "@langchain/langgraph-sdk";
 
 export const createClient = () => {
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api";
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "https://storia-cdd2862137c25b7dbbd31df228a10388.us.langgraph.app";
   return new Client({
     apiUrl,
+    apiKey: process.env.LANGCHAIN_API_KEY || "",
   });
 };
